523a1777011514886bbcba5605f7c6dbac1ddc70
dgbogi564 <82741370+dgbogi564@users.noreply.github.com>
2022-04-22 14:10:18 -0400

    benchmark.sh: Minor changes

--- benchmark.sh
+++ benchmark.sh
@@ -2,19 +2,21 @@
 
 make clean
 make default || exit
+rm -rf out
 mkdir -p out
+rm -rf sol-out
 mkdir -p sol-out
 
-printf "\n\n"
+printf "\n"
 
 readarray -d '' testcases < <(printf '%s\0' ./testcases/* | sort -zV)
 for testcase in "${testcases[@]}"; do
 
-    ./sol-codegen < "$testcase" > "./sol-out/${testcase##*/}.log"
+    ./sol-codegen < "$testcase" >> "./sol-out/${testcase##*/}.log" 2>&1
     mv "iloc.out" "./sol-out/${testcase##*/}.iloc"
     ~uli/cs415/ILOC_Simulator/sim < "./sol-out/${testcase##*/}.iloc" > "./sol-out/${testcase##*/}.out"
 
-    ./codegen < "$testcase" > "./out/${testcase##*/}.log"
+    ./codegen < "$testcase" >> "./out/${testcase##*/}.log" 2>&1
     mv "iloc.out" "./out/${testcase##*/}.iloc"
     ~uli/cs415/ILOC_Simulator/sim < "./out/${testcase##*/}.iloc" > "./out/${testcase##*/}.out"
 
